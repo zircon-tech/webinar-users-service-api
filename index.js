03924a536b453e83b4ba0e3d5931fed1fe6a0c72
Will Olivera <wolivera@zircon.tech>
2024-10-11 12:44:28 -0300

    adding bob to the list of users

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 exports.handler = async (event) => {
+    // TODO: Grab this users from a databse
     const users = [
         { id: 1, name: 'John Doe', email: 'john.doe@example.com' },
         { id: 2, name: 'Jane Smith', email: 'jane.smith@example.com' },
-        { id: 3, name: 'Alice Johnson', email: 'alice.johnson@example.com' }
+        { id: 3, name: 'Alice Johnson', email: 'alice.johnson@example.com' },
+        { id: 3, name: 'Bob Brown', email: 'bob.brown@example.com' },
     ];
+    let user = users.find(user => user.id === 1);
 
     const response = {
         statusCode: 200,
