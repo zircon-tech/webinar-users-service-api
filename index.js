b89acbf23728444dcae4f4b47f076d5bdf1d37b7
Will <wolivera@zircon.tech>
2024-10-14 15:12:13 -0300

    added extra user

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 exports.handler = async (event) => {
+    // TODO fetch users from dtbase
     const users = [
         { id: 1, name: 'John Doe', email: 'john.doe@example.com' },
         { id: 2, name: 'Jane Smith', email: 'jane.smith@example.com' },
-        { id: 3, name: 'Alice Johnson', email: 'alice.johnson@example.com' }
+        { id: 3, name: 'Alice Johnson', email: 'alice.johnson@example.com' },
+        { id: 3, name: 'Bob Brown', email: 'bob@mail.com' },
     ];
 
     const response = {
