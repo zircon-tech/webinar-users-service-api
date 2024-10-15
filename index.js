d946725901620b066a514fddb37a7d43768a0fbc
Will <wolivera@zircon.tech>
2024-10-15 15:59:43 -0300

    reset

--- index.js
+++ index.js
@@ -1,12 +1,9 @@
 exports.handler = async (event) => {
-    // TODO: fetch these users from the databse
     const users = [
         { id: 1, name: 'John Doe', email: 'john.doe@example.com' },
         { id: 2, name: 'Jane Smith', email: 'jane.smith@example.com' },
-        { id: 3, name: 'Alice Johnson', email: 'alice.johnson@example.com' },
-        { id: 3, name: 'Bob Johnson', email: 'bob@mail.com' }
+        { id: 3, name: 'Alice Johnson', email: 'alice.johnson@example.com' }
     ];
-    user = users.filter(user => user.name === 'Bob Johnson');
 
     const response = {
         statusCode: 200,
