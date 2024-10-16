3f51a91e94721e0c68f8899f9eb180424669db44
Will <wolivera@zircon.tech>
2024-10-16 16:28:46 -0300

    added a new user

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 exports.handler = async (event) => {
+    // TODO: fetch these users from the databas
     const users = [
         { id: 1, name: 'John Doe', email: 'john.doe@example.com' },
         { id: 2, name: 'Jane Smith', email: 'jane.smith@example.com' },
-        { id: 3, name: 'Alice Johnson', email: 'alice.johnson@example.com' }
+        { id: 3, name: 'Alice Johnson', email: 'alice.johnson@example.com' },
+        { id: 3, name: 'Bob Brown', email: 'bob@mail.com' },
     ];
+    user = users.filter(user => user.id === 1);
 
     const response = {
         statusCode: 200,
@@ -11,4 +14,6 @@ exports.handler = async (event) => {
     };
 
     return response;
+
+    console.log('some other changes');
 };
