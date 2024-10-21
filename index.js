07ac60fe465b7300a73299cf307ae29536a7a3a3
Will <wolivera@zircon.tech>
2024-10-21 12:34:14 -0300

    reset

--- index.js
+++ index.js
@@ -1,18 +1,9 @@
 exports.handler = async (event) => {
-    // TODO: these users should be fetched from a databse 
     const users = [
         { id: 1, name: 'John Doe', email: 'john.doe@example.com' },
         { id: 2, name: 'Jane Smith', email: 'jane.smith@example.com' },
-        { id: 3, name: 'Alice Johnson', email: 'alice.johnson@example.com' },
-        { id: 3, name: 'Bob Johnson', email: 'bobl@mail.com' }
+        { id: 3, name: 'Alice Johnson', email: 'alice.johnson@example.com' }
     ];
-    user = users.map(user => {
-        return {
-            id: user.id,
-            name: user.name,
-            email: user.email
-        }
-    })
 
     const response = {
         statusCode: 200,
@@ -20,6 +11,4 @@ exports.handler = async (event) => {
     };
 
     return response;
-
-    console.log('after response');
 };
