6d2954d831bc2fe402be438550b4f1aae0fc8bb1
Will <wolivera@zircon.tech>
2024-10-22 14:54:15 -0300

    reset

--- index.js
+++ index.js
@@ -1,18 +1,9 @@
 exports.handler = async (event) => {
-    // TODO return these users from a databse 
     const users = [
         { id: 1, name: 'John Doe', email: 'john.doe@example.com' },
         { id: 2, name: 'Jane Smith', email: 'jane.smith@example.com' },
-        { id: 3, name: 'Alice Johnson', email: 'alice.johnson@example.com' },
-        { id: 3, name: 'Bob Brown', email: 'bob@mail.com' },
+        { id: 3, name: 'Alice Johnson', email: 'alice.johnson@example.com' }
     ];
-    user = users.map(user => {
-        return {
-            id: user.id,
-            name: user.name,
-            email: user.email
-        }
-    });
 
     const response = {
         statusCode: 200,
@@ -20,6 +11,4 @@ exports.handler = async (event) => {
     };
 
     return response;
-
-    console.log('unreachable code');
 };
