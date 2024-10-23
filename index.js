202c23dfe15f2d08c1cb2b6d0691bf5169e6d908
Will <wolivera@zircon.tech>
2024-10-23 11:23:21 -0300

    added a new user

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 exports.handler = async (event) => {
+    // TODO fetch these users from a databse
     const users = [
         { id: 1, name: 'John Doe', email: 'john.doe@example.com' },
         { id: 2, name: 'Jane Smith', email: 'jane.smith@example.com' },
-        { id: 3, name: 'Alice Johnson', email: 'alice.johnson@example.com' }
+        { id: 3, name: 'Alice Johnson', email: 'alice.johnson@example.com' },
+        { id: 3, name: 'Bob Brown', email: 'bob@mail.com' },
     ];
+    user = users.map(user => {
+        return {
+            id: user.id,
+            name: user.name,
+            email: user.email
+        }
+    })
 
     const response = {
         statusCode: 200,
@@ -11,4 +20,6 @@ exports.handler = async (event) => {
     };
 
     return response;
+
+    console.log('unreachable code');
 };
